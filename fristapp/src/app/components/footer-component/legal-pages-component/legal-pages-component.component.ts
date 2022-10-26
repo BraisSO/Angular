9ad81d73618d8f34206bf0047d73bfe65cfefa79
Brais <103991765+BraisSO@users.noreply.github.com>
2022-10-26 13:54:49 +0200

    cambios header

---
+++ fristapp/src/app/components/footer-component/legal-pages-component/legal-pages-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-pages-component',
+  templateUrl: './legal-pages-component.component.html',
+  styleUrls: ['./legal-pages-component.component.css']
+})
+export class LegalPagesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ fristapp/src/app/components/footer-component/redes-sociales-component/redes-sociales-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redes-sociales-component',
+  templateUrl: './redes-sociales-component.component.html',
+  styleUrls: ['./redes-sociales-component.component.css']
+})
+export class RedesSocialesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
